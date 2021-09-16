ea8e2f8b0d141ee93c13d829448b181fe96d4ac8
Vaibhav <vaibhavagrawal093@gmail.com>
2021-09-16 09:13:29 +0530

    navbaradded

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,15 @@
-import Navbar from './components/Navbar';
+import Header from './components/header/Header';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Skill from './components/Skill';
 
 function App() {
-  //const title = 'Welcome to project';
-  //const person={name :'Vaibhav' ,age:30};
+ 
 
   return (
     <div className="App">
-      <Navbar />
+      <Header />
       <Home />
       <Skill />
       <Projects />
---
+++ portfolio/src/components/header/Header.js
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+import './header.css';
+import { HiMenuAlt3,HiOutlineX } from "react-icons/hi";
+
+
+const Header = () => {
+    const [showNav,SetshowNav] = useState(false); 
+    return (
+     <nav className='navbar'>
+      <div className='navbar__main'>
+        <h1 className="navname"> Vaibhav </h1>
+        <ul className='navbar__list--desktop'>
+          <li><a href='#about'>About</a></li>
+          <li><a href='#about'>Projects</a></li>
+          <li><a href='#projects'>Skills</a></li>
+          <li><a href='#contact'>Contact</a></li>
+        </ul>
+        <span className="navbar__mobile--icon" onClick={() => SetshowNav(prev => !prev)}>{showNav ? <HiOutlineX /> : <HiMenuAlt3 />}</span>
+        <ul className={showNav ? 'navbar__list--mobile navbar__list--mobileShow' : 'navbar__list--mobile'}>
+          <li><a href='#about'>About</a></li>
+           <li><a href='#about'>Projects</a></li>
+          <li><a href='#projects'>Skills</a></li>
+          <li><a href='#contact'>Contact</a></li>
+        </ul>
+        </div>
+      </nav>
+     
+    )
+}
+
+export default Header;
